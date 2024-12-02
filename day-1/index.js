78e94ada9733253f92f0023b42227b66a57442df
Andrew Hudson <ahudson@gmail.com>
2024-12-02 19:50:22 +0100

    day-1

---
+++ day-1/index.js
@@ -0,0 +1,44 @@
+// import { testInput } from "./test.js";
+import { actualInput } from "./real.js";
+
+// testCode
+// const rows = testInput.split("\n").filter((l) => l !== "");
+const rows = actualInput.split("\n").filter((l) => l !== "");
+
+const firstArray = [];
+const secondArray = [];
+rows.forEach((line) => {
+  const [first, second] = line.split("   ");
+  firstArray.push(first);
+  secondArray.push(second);
+});
+// sort arrays by order
+const sortedFirst = firstArray.sort();
+const sortedSecond = secondArray.sort();
+
+// Go through arrays at the same time, smallest to biggest (sort did this), work out diff for each pairing, add to total.
+
+// Use Reduce for this
+const firstAnswer = sortedFirst.reduce(
+  (accumulator, currentValue, currentIndex) => {
+    const diff = currentValue - sortedSecond[currentIndex];
+    return accumulator + Math.abs(diff);
+  },
+  0
+);
+
+console.log("PART ONE ANSWER:", firstAnswer);
+
+const secondAnswer = sortedFirst.reduce((accumulator, currentValue) => {
+  let count = 0;
+  // count the number of times the currentValue is in the secondArray.
+  sortedSecond.forEach((n) => {
+    if (n === currentValue) {
+      count++;
+    }
+  });
+
+  return accumulator + count * currentValue;
+}, 0);
+
+console.log({ secondAnswer });
---
+++ day-1/test.js
@@ -0,0 +1,8 @@
+export const testInput = `
+3   4
+4   3
+2   5
+1   3
+3   9
+3   3
+`;
