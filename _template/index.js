4b69effa70f90f27b3c396158d6a18c861c75643
Andrew Hudson <ahudson@gmail.com>
2024-12-03 08:56:25 +0100

    create template folder

---
+++ _template/index.js
@@ -0,0 +1,40 @@
+import { testInput } from "./test.js";
+import { actualInput } from "./real.js";
+
+// testCode
+const rows = testInput.split("\n").filter((l) => l !== "");
+// .slice(0, 1);
+// Actual Input
+// const rows = actualInput.split("\n").filter((l) => l !== "");
+// .slice(0, 1);
+// const rows = actualInput.split("\n").filter((l) => l !== "");
+
+const firstAnswer = () => {
+  let count = 0;
+  rows.forEach((row) => {
+    const numberArray = row.split(" ").map((n) => Number(n));
+
+    const ans = compareNumbersInArray(numberArray);
+
+    const passes = checkIfPasses(ans);
+
+    if (passes) {
+      count++;
+    }
+  });
+
+  console.log("part-1 answer:", count);
+};
+
+firstAnswer();
+
+// const secondAnswer = () => {
+//   let count = 0;
+//   rows.forEach((row) => {
+
+//   });
+
+//   console.log("part-2 answer:", count);
+// };
+
+// secondAnswer();
